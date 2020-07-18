f215ab316516ed7609a413dd8a17e7ae049d0ea4
John Karima <karimajohn24@gmail.com>
2020-07-18 08:49:29 +0300

    add bootstrap css and link index.html to js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function addMachine(){
+  var numero1 = parseInt(document.getElementById("num1").value);
+  var numero2 = parseInt(document.getElementById("num2").value);
+
+  alert(numero1 + numero2)}
+
+//   var answerCalc = numero1 + numero2;
+//   alert(answerCalc);
+// 
