74f6f0ec4a7a6a768a3e62ae537c3acb9455a398
John Karima <karimajohn24@gmail.com>
2020-07-18 09:06:34 +0300

    divide year input into two for better passing into formula

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,20 @@ var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var AkanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
 function addMachine(){
-  var year = parseInt(document.getElementById("year").value);
+  var year = document.getElementById("year").value;
   var month = parseInt(document.getElementById("month").value);
   var day = parseInt(document.getElementById("day").value);
+  var cc = parseInt(year.slice(0,2));
+  var yy = parseInt(year.slice(2,4));
+
+  alert(cc)
+  alert(yy)
+
+
+}
+
+
+  // (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 
   //alert(year + month + day)}
 
